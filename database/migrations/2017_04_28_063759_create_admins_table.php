e8a4eb83a9efa042bf13524c59a531499a3e2439
ththth0303 <trungthangngo@gmail.com>
2017-04-28 14:23:50 +0700

    add project and 1/2 migrate

---
+++ database/migrations/2017_04_28_063759_create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('f_name');
+            $table->string('l_name');
+            $table->string('phone');
+            $table->string('address');
+            $table->integer('rule')->unsigned();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
