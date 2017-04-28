73ef098f9ef33ff7653ec79a3b0a5f77c61c5c2f
tuanlabophp <tuan.nguyenducanh@gmail.com>
2017-04-28 14:59:06 +0700

    fix migration

--- database/migrations/2017_04_28_063306_create_products_table.php
+++ database/migrations/2017_04_28_063306_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->integer('category_id');
             $table->decimal('price', 10, 0);
             $table->float('sale_percent');
