dcacf1aaa0d5bbed3c5f6c7d8fad4f85ab0f70e2
ththth0303 <trungthangngo@gmail.com>
2017-05-05 09:03:06 +0700

    bower

--- database/migrations/2017_04_28_082223_foreignkey.php
+++ database/migrations/2017_04_28_082223_foreignkey.php
@@ -16,31 +16,45 @@ class Foreignkey extends Migration
         Schema::table('rates', function (Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users');
         });
+        Schema::table('rates', function (Blueprint $table) {
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+
         Schema::table('wishlists', function (Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users');
         });
+
         Schema::table('comments', function (Blueprint $table) {
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')
         });
+
         Schema::table('orders', function (Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users');
         });
-        Schema::table('products', function (Blueprint $table) {
-            $table->foreign('created_by')->references('id')->on('admins');
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreign('deliver_id')->references('id')->on('delivers');
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreign('shipper_id')->references('id')->on('shippers');
         });
 
         Schema::table('products', function (Blueprint $table) {
+            $table->foreign('created_by')->references('id')->on('admins');
+        });
+        Schema::table('products', function (Blueprint $table) {
             $table->foreign('category_id')->references('id')->on('categories');
         });
+
         Schema::table('cate_product_details', function (Blueprint $table) {
             $table->foreign('category_id')->references('id')->on('categories');
         });
-       Schema::table('order_details', function (Blueprint $table) {
+
+        Schema::table('order_details', function (Blueprint $table) {
             $table->foreign('order_id')->references('id')->on('orders');
         });
         Schema::table('order_details', function (Blueprint $table) {
             $table->foreign('product_id')->references('id')->on('products');
-        }); 
+        });
     }
 
     /**
